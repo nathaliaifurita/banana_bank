df967f95b1774c87f92dc5d7595f94f27b1404e6
Nathalia Freire <nathaliaofreire@gmail.com>
2023-07-28 15:28:13 -0300

    60. Creating the action show

--- lib/banana_bank/users.ex
+++ lib/banana_bank/users.ex
@@ -1,5 +1,6 @@
 defmodule BananaBank.Users do
-  alias BananaBank.Users.Create
+  alias BananaBank.Users.{Create, Get}
 
   defdelegate create(params), to: Create, as: :call
+  defdelegate get(params), to: Get, as: :call
 end
