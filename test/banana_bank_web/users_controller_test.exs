26b2cb00b0e6202a9a54d0f3f7376e737f8652cc
Nathalia Freire <nathaliaofreire@gmail.com>
2023-08-10 21:58:55 -0300

    67. Testing Controller pt3

--- test/banana_bank_web/users_controller_test.exs
+++ test/banana_bank_web/users_controller_test.exs
@@ -1,5 +1,7 @@
 defmodule BananaBankWeb.UsersControllerTest do
   use BananaBankWeb.ConnCase
+  alias BananaBank.Users
+  alias Users.User
 
   describe "create/2" do
     test "successfully creates an user", %{conn: conn} do
@@ -35,6 +37,31 @@ defmodule BananaBankWeb.UsersControllerTest do
         conn
         |> post(~p"/api/users", params)
         |> json_response(:bad_request)
+
+      assert response == expected_response
+    end
+  end
+
+  describe "delete/2" do
+    test "successfully deletes an user", %{conn: conn} do
+      params = %{
+        name: "Nath",
+        cep: "12345678",
+        email: "nat@test.com",
+        password: "123456"
+      }
+
+      {:ok, %User{id: id}} = Users.create(params)
+
+      response =
+        conn
+        |> delete(~p"/api/users/#{id}")
+        |> json_response(:ok)
+
+      expected_response = %{
+        "data" => %{"cep" => "12345678", "email" => "nat@test.com", "id" => id, "name" => "Nath"},
+        "message" => "User excluído com sucesso!"
+      }
 
       assert response == expected_response
     end
