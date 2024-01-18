1b59dd3414c44d319eb2c7a47b9025312fbe316a
Nathalia Freire <nathaliaofreire@gmail.com>
2024-01-18 11:26:32 -0300

    81. Creating transactions with Ecto Multi

--- lib/banana_bank/accounts/create.ex
+++ lib/banana_bank/accounts/create.ex
@@ -3,8 +3,8 @@ defmodule BananaBank.Accounts.Create do
   alias BananaBank.Repo
 
   def call(params) do
-      params
-      |> Account.changeset()
-      |> Repo.insert()
+    params
+    |> Account.changeset()
+    |> Repo.insert()
   end
 end
